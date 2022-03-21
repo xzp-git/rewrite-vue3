c1c2fb27516f5c9c07eda0cb4e80f08321aa3117
“xzpCSCKFWMS <l>
2022-03-21 23:30:11 +0800

    dist 推送dist

---
+++ packages/reactivity/dist/reactivity.global.js
@@ -0,0 +1,110 @@
+var VueReactivity = (() => {
+  var __defProp = Object.defineProperty;
+  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __hasOwnProp = Object.prototype.hasOwnProperty;
+  var __export = (target, all) => {
+    for (var name in all)
+      __defProp(target, name, { get: all[name], enumerable: true });
+  };
+  var __copyProps = (to, from, except, desc) => {
+    if (from && typeof from === "object" || typeof from === "function") {
+      for (let key of __getOwnPropNames(from))
+        if (!__hasOwnProp.call(to, key) && key !== except)
+          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+    }
+    return to;
+  };
+  var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+  // packages/reactivity/src/index.ts
+  var src_exports = {};
+  __export(src_exports, {
+    computed: () => computed,
+    effect: () => effect,
+    reactive: () => reactive
+  });
+
+  // packages/reactivity/src/computed.ts
+  var computed = () => {
+  };
+
+  // packages/reactivity/src/effect.ts
+  var activeEffect = void 0;
+  var ReactiveEffect = class {
+    constructor(fn, scheduler) {
+      this.fn = fn;
+      this.scheduler = scheduler;
+      this.deps = [];
+      this.active = true;
+    }
+    run() {
+      if (!this.active) {
+        return this.fn();
+      }
+      try {
+        this.parent = activeEffect;
+        activeEffect = this;
+        return this.fn();
+      } finally {
+        activeEffect = this.parent;
+      }
+    }
+  };
+  var effect = (fn, options = {}) => {
+    const _effect = new ReactiveEffect(fn, options.scheduler);
+    _effect.run();
+    const runner = _effect.run.bind(_effect);
+    runner.effect = _effect;
+    return runner;
+  };
+
+  // packages/shared/src/index.ts
+  var isObject = (value) => {
+    return typeof value === "object" && value !== null;
+  };
+  var isArray = Array.isArray;
+
+  // packages/reactivity/src/baseHandler.ts
+  var mutableHandlers = {
+    get(target, key, receiver) {
+      if (key === "__v_isReactive" /* IS_REACTIVE */) {
+        return true;
+      }
+      console.log("\u6536\u96C6\u4F9D\u8D56");
+      let res = Reflect.get(target, key, receiver);
+      if (isObject(res)) {
+        return reactive(res);
+      }
+      return res;
+    },
+    set(target, key, value, receiver) {
+      let oldValue = target[key];
+      let result = Reflect.set(target, key, value, receiver);
+      if (oldValue !== value) {
+        console.log("\u89E6\u53D1\u66F4\u65B0");
+      }
+      return result;
+    }
+  };
+
+  // packages/reactivity/src/reactivity.ts
+  var reactiveMap = /* @__PURE__ */ new WeakMap();
+  var reactive = (target) => {
+    if (!isObject(target)) {
+      return;
+    }
+    if (target["__v_isReactive" /* IS_REACTIVE */]) {
+      return target;
+    }
+    let exisitingProxy = reactiveMap.get(target);
+    if (exisitingProxy) {
+      return exisitingProxy;
+    }
+    const proxy = new Proxy(target, mutableHandlers);
+    reactiveMap.set(target, proxy);
+    return proxy;
+  };
+  return __toCommonJS(src_exports);
+})();
+//# sourceMappingURL=reactivity.global.js.map
